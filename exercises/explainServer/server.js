83ef261456d83526a4f33a5f32babe042e4d0a4a
Adrian <serbanedward@yahoo.com>
2024-11-23 14:13:29 +0100

    update

--- exercises/explainServer/server.js
+++ exercises/explainServer/server.js
@@ -1,26 +1,23 @@
-// Part 1
-import express from 'express';
+// hello
+import express from "express";
 
 // Part 2
-const app = express(); 
-
+const app = express();
 
 // Part 3
 app.use(bodyParser.json());
 
-
 // Part 4
-app.get('/', (req, res) => {
-    res.send('Hi');
-  });
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
 
-app.post('/login', (req, res) => {
-    console.log(req.body);
-    res.send('Ok');
-  });
-
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
 
 // Part 5
 app.listen(3000, () => {
-  console.log('Server läuft auf http://localhost:3000');
+  console.log("Server läuft auf http://localhost:3000");
 });
